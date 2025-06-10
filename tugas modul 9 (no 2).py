d630dc15541d0c7e52124e068499a11cd05b7b1e
meisyaputrijenicka <aaachcha97@gmail.com>
2025-06-10 22:10:44 +0700

    Add files via upload

---
+++ tugas modul 9 (no 2).py
@@ -0,0 +1,21 @@
+import os
+from termcolor import cprint
+
+os.system("cls")
+print("LOGO ADIDAS")
+
+cprint("                                                                █","black")
+cprint("                                                              ████","black")
+cprint("                                                            ███████","black")
+cprint("                                                       █     ███████","black")
+cprint("                                                     ████     ███████","black")
+cprint("                                                   ███████     ███████","black")
+cprint("                                              █     ███████     ███████","black")
+cprint("                                            ████     ███████     ███████","black")
+cprint("                                          ███████     ███████     ███████","black")
+cprint("                                           ███████     ███████     ███████\n","black")
+cprint("                                                       █   ██        █","black")
+cprint("                                          ████     █████         █████     ████   █████ ","black")
+cprint("                                         █    █   █    █   ██   █     █   █    █  ██","black")
+cprint("                                         █    █   █    █   ██   █     █   █    █    ███","black")
+cprint("                                          █████    █████   ██    █████     █████  █████","black")
