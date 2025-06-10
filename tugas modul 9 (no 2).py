04894663681ba9e5bc547c0366c901c20db8eec5
meisyaputrijenicka <aaachcha97@gmail.com>
2025-06-10 23:48:16 +0700

    tugas modul 9 (no 2)ok.py

--- tugas modul 9 (no 2).py
+++ tugas modul 9 (no 2).py
@@ -4,18 +4,32 @@ from termcolor import cprint
 os.system("cls")
 print("LOGO ADIDAS")
 
-cprint("                                                                █","black")
-cprint("                                                              ████","black")
-cprint("                                                            ███████","black")
-cprint("                                                       █     ███████","black")
-cprint("                                                     ████     ███████","black")
-cprint("                                                   ███████     ███████","black")
-cprint("                                              █     ███████     ███████","black")
-cprint("                                            ████     ███████     ███████","black")
-cprint("                                          ███████     ███████     ███████","black")
-cprint("                                           ███████     ███████     ███████\n","black")
-cprint("                                                       █   ██        █","black")
-cprint("                                          ████     █████         █████     ████   █████ ","black")
-cprint("                                         █    █   █    █   ██   █     █   █    █  ██","black")
-cprint("                                         █    █   █    █   ██   █     █   █    █    ███","black")
-cprint("                                          █████    █████   ██    █████     █████  █████","black")
+logo_stripes_data = [
+  "                           ",
+    "                                ###",
+    "                            ##########",
+    "                         #################",
+    "                            ################",
+    "                       ###     ################ ",
+    "                   ##########    ################",
+    "                ################    ################",
+    "                  ################    ################",
+    "         ###        ################    ################",
+    "      #########       ################    ################",
+    "     #############      ################    ################",
+    "        #############     ################     ################",
+]
+
+for baris in logo_stripes_data:
+    for piksel in baris:
+        if piksel == '#':
+            cprint(' ', on_color='on_black', end='')
+        else:
+            cprint(' ', color='white', end='')
+    print("\n")
+
+cprint("                        █   ██        █","black")
+cprint("           ████     █████         █████     ████   █████ ","black")
+cprint("          █    █   █    █   ██   █     █   █    █  ██","black")
+cprint("          █    █   █    █   ██   █     █   █    █    ███","black")
+cprint("           █████    █████   ██    █████     █████  █████","black")
